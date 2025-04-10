885d1c647a4a4dc4109aabbb7fa4dc7bced46af3
Ron Mordukhovich <ronmordo1@outlook.com>
2025-04-10 20:16:49 +0300

    Force scroll to top,

--- src/components/scrollToTop/ScrollToTop.jsx
+++ src/components/scrollToTop/ScrollToTop.jsx
@@ -5,7 +5,13 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
+    if ("scrollRestoration" in window.history) {
+      window.history.scrollRestoration = "manual";
+    }
+
     setTimeout(() => {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
       window.scrollTo(0, 0);
     }, 50);
   }, [pathname]);
